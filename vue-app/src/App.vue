ca8f47ec03df5eb4a46aea03617228d7a0402241
Yeongseon Choe <ychoe@microsoft.com>
2021-03-18 00:16:49 +0900

    Updated App.vue

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Azure app service - vue!"/>
+    <HelloWorld msg="Azure app service - vue"/>
   </div>
 </template>
 
